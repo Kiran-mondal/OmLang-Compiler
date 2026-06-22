24e8c41ba2f08d4909af36b2701ac835020775e5
Kiran Mondal <kiranmondal5516@gmail.com>
2026-06-22 15:44:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
 
 setup(
-    name="om-lang",
-    version="1.2.0",
-    py_modules=["om"],
+    name='OmLang-Compiler',
+    version='2.6.0',
+    description='A clean, friendly, cross-platform standalone programming language for beginners.',
+    author='Kiran Mondal',
+    py_modules=['om'],
     entry_points={
         'console_scripts': [
             'om=om:cli',
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
